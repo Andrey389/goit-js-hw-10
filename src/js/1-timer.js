4de51e503594db8a8a3e946f677d47f1479c27c1
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-03-27 20:31:04 +0200

    add

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,20 +1,19 @@
-// commonjs
-const flatpickr = require('flatpickr');
-
-// es modules are recommended, if available, especially for typescript
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const fp = document.querySelector('#datetime-picker')._flatpickr;
 let intervalId = null;
 const startBtn = document.querySelector('[data-action-start]');
 
-flatpickr('#datetime-picker', options);
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+// flatpickr('#datetime-picker', options);
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
