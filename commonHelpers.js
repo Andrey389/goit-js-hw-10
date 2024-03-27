1d75da61174bb1773bb3de979da378ee0bcd48e3
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-03-27 19:07:41 +0000

    Deploying to gh-pages from @ Andrey389/goit-js-hw-10@d439df24d583c85f79dc712d6e51f1f9c9ac2423 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-f3635d75.js";import{f as t}from"./assets/vendor-4daf66c6.js";document.querySelector("#datetime-picker")._flatpickr;document.querySelector(".btn-start");const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",r);
+//# sourceMappingURL=commonHelpers.js.map
