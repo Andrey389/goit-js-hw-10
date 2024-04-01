c033bf114d7caa10b28aa390e259cc55239bd42f
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-04-01 22:18:04 +0300

    add

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,23 @@
 import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
+
+const delayEl = document.querySelector('.delay-input');
+const fulfilledEl = document.querySelector('.ful-input');
+const rejectEl = document.querySelector('.rej-input');
+const btnSubEl = document.querySelector('.btn-submit');
+
+function createPromise(delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (fulfilledEl) {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+  return promise;
+}
+const p1 = createPromise(1000);
+console.log(p1);
